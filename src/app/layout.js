c0bd0ff0cb9c429778b400aa76e06a90cae2f9cb
Bexruz21 <rakhmanovbexruz@gmail.com>
2025-10-08 15:31:05 +0500

    1.1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
 import "./globals.css";
@@ -6,6 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <SpeedInsights/>
         <div className="min-h-screen bg-gradient-to-br from-white via-blue-50 to-cyan-50 flex flex-col">
           <Navigation />
           <main className="flex-1">
